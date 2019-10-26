52b948a326617f449b0753f6e567e6baa9543120
choymoon <choymoonk@gmail.com>
2019-10-26 16:07:43 +0900

    initial commit

---
+++ INS-UP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INS-UP
+//
+//  Created by Cho on 26/10/2019.
+//  Copyright © 2019 Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
